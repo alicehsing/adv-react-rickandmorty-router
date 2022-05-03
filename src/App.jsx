ac0d7a7016c981d14dec1dee3d5b2b0ec07952cf
Alice Hsing <alicehsing@gmail.com>
2022-05-02 17:37:59 -0700

    rendered a list of characters from API fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Detail from './views/Characters/Detail';
 import CharacterList from './views/Characters/List';
+import './App.css';
 
 export default function App() {
   return (
@@ -10,11 +11,9 @@ export default function App() {
         <Route path="/characters/:id">
           <Detail />
         </Route>
-        <Route path="/characters">
-          <CharacterList />
-        </Route>
         <Route path="/">
           <Header />
+          <CharacterList />
         </Route>
       </Switch>
     </>
--- src/views/Characters/List.jsx
+++ src/views/Characters/List.jsx
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../../App.css';
 
-export default function List() {
-  return <div>List</div>;
+export default function CharacterList() {
+  const [loading, setLoading] = useState(true);
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+      setLoading(true);
+      const res = await fetch('https://rickandmortyapi.com/api/character');
+      const { results } = await res.json();
+      console.log(results);
+      setCharacters(results);
+      setLoading(false);
+    };
+    fetchCharacters();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <p>Loading characters...</p>
+      ) : (
+        <>
+          <div className={styles.list}>
+            <h2>Character List</h2>
+            {characters.map((character) => {
+              return (
+                <section key={character.id}>
+                  <Link to={`/characters/${character.id}`}>
+                    <h3>{character.name}</h3>
+                  </Link>
+                  <span>Species: {character.species}</span>
+                  <p>Status: {character.status}</p>
+                  <p>Location: {character.location.name}</p>
+                </section>
+              );
+            })}
+          </div>
+        </>
+      )}
+    </>
+  );
 }
